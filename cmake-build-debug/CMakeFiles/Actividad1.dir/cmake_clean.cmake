f0525a8ee1e77e8717ae319ef1d16b42039dafe1
Valeria <valeriamorales40@gmail.com>
2020-03-12 04:28:32 -0600

    init

---
+++ cmake-build-debug/CMakeFiles/Actividad1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Actividad1"
+  "Actividad1.pdb"
+  "CMakeFiles/Actividad1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Actividad1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

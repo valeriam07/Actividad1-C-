89e0f35942edeb30d92d03428a3526b0a7b3c8df
Valeria <valeriamorales40@gmail.com>
2020-03-14 22:52:40 -0600

    encrypted token added

--- main.cpp
+++ main.cpp
@@ -7,3 +7,17 @@ int main(){
 int sumar(int x, int y){
     return x+y;
 }
+
+int restar(int x, int y){
+    return x-y;
+}
+
+
+int multiplicar(int x, int y){
+    return x*y;
+}
+
+
+int dividir(int x, int y){
+    return x/y;
+}
